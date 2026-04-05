6a0fc3cac4589498d139ed8cf618e7c46cfbdd7c
Jairo Talancon <jairotalancon@icloud.com>
2026-04-05 12:56:14 -0700

    replace bcrypt with bcryptjs and ignore node_modules

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { Server } = require("socket.io");
 
 const app = express();
